d4dd89accbe27ab46e7f2a252fa3829290672832
Аксентий Виктория Георгиевна <vgaksentii@domclick.ru>
2021-11-21 00:05:37 +0300

    выгрузка данных из csv файлов в postgresql

--- README.md
+++ README.md
@@ -2,4 +2,20 @@
 Перенос данных из SQLite в Postgres
 
 
-Запустить контейнер с Postgresql и спроектировать БД [контейнер и инструкция тут](https://github.com/wiky-avis/psql-container.git)
+Запустить контейнер с Postgresql и спроектировать БД для переноса [контейнер и инструкция тут](https://github.com/wiky-avis/psql-container.git).
+
+Установите виртуальное окружение и активируйте его:
+    `python -m venv venv`
+    
+    `source venv/bin/activate`
+
+Установите зависимости:
+
+    `pip install -r requirements.txt`
+
+Запустите скрипт:
+
+    `python sql_to_psql.py`
+
+
+После запуска скрипт автоматически создаст папку "data" и выгрузит данные из базы SQLite в csv файлы, затем загрузит в базу Postgresql.
--- sql_to_psql.py
+++ sql_to_psql.py
@@ -1,8 +1,5 @@
-import io
 import os
-import sqlite3
 import psycopg2
-import subprocess
 
 psql_dsn = {
  'dbname': 'moviesdb',
@@ -22,3 +19,11 @@ for table_name in tables_names:
      "sqlite3 -header -csv db.sqlite 'SELECT DISTINCT * FROM {0};' > data/{1}.csv".format(table_name, table_name)
     )
 
+# загружаем данные из csv файлов в postgresql
+with psycopg2.connect(**psql_dsn) as conn, conn.cursor() as cursor:
+    for table_name in tables_names:
+        # Очищаем таблицу в БД, чтобы загружать данные в пустую таблицу
+        cursor.execute("""TRUNCATE content.{0}""".format(table_name))
+        with open('data/{0}.csv'.format(table_name), "r") as csv_file_name:
+            sql = "COPY content.{0} FROM STDIN DELIMITER ',' CSV HEADER".format(table_name)
+            cursor.copy_expert(sql, csv_file_name)
